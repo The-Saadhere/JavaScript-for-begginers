32199118a72890911c2800d10cca9773b3e32e2c
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2026-06-27 22:38:26 +0500

    Added Variable Tutorial

---
+++ 02_JavaScript Variables/script.js
@@ -0,0 +1,60 @@
+console.log("Hey I am SaadkamalShaikh I am Very Good at Learning")
+let a = 10;
+a = a + 100;
+let b = 5.5;
+let c = "SAAd";
+let d = true
+var _saad = "this is Hardwork you Know How Hard it is?"
+console.log(a + b + 15 + c +  _saad)
+console.log(typeof a, typeof b,typeof c, typeof d)
+// const a1 = 12;
+// a1 = a1 + 1;
+// console.log(a1) mana hy ye kam nahi karega 
+let abc = 12;
+abc = abc**4;
+console.log(abc)
+{
+    let a =500;
+    console.log(a)
+}
+console.log(a)
+let u = "SaadkamalShaikh";
+let k = 30;
+let m = 12.1;
+const n = true;
+let v = null;
+let r = undefined;
+let x = "%^"
+console.log(u, k, m, n, v, r, x)
+console.log(typeof u, typeof k, typeof m, typeof n, typeof v, typeof r, typeof x)
+let o ={
+    "Saadkamalshaikh": "webdeveloper",
+    "Job Role": 100000,
+    "Is good": true
+}
+console.log(o);
+o.salary = "100lakh";
+console.log(o);
+o.salary = "1400lakh";
+console.log(o);
+const person ={
+    "name": "SaadkamalShaikh",
+    "age" : 16,
+    "profession": "Webdeveloper"
+}
+console.log(person);
+person.profession = "Software Engineer";
+console.log(person);
+person.Salary = "10crore";
+console.log(person);
+const mean={
+    "dog": "kuta",
+    "human" : "Insaan",
+    "AI": "Job Khatry me",
+    "Cat": "Billi",
+    "genius": "Samagh dar ya 120 IQ+"
+}
+console.log(mean); 
+let square = 14;
+square = square12;
+console.log(square)
